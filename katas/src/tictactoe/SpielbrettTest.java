4ce5a4adcdf2b2bdd3d5cc7f9577dda4e75dae35
Jannis von Albedyll <jannis.vonalbedyll@posteo.de>
2019-02-26 10:27:04 +0100

    Unit tests added

--- katas/src/tictactoe/SpielbrettTest.java
+++ katas/src/tictactoe/SpielbrettTest.java
@@ -15,13 +15,9 @@ public class SpielbrettTest {
 
 	@Test
 	public void initialization() {
-		int[][] expected = new int[][]{ {0, 0, 0},
-										{0, 0, 0},
-										{0, 0, 0} };
-				
 		int[][] actual = spielbrett.ausgabe();
 		
-		assertArrayEquals(expected, actual);
+		assertArrayEquals(getInitialField(), actual);
 	}
 	
 	@Test
@@ -33,9 +29,54 @@ public class SpielbrettTest {
 		spielbrett.zug(0, 1);
 		spielbrett.zug(1, 2);
 		
-		int[][] actual = spielbrett.ausgabe();
-		
-		assertArrayEquals(expected, actual);
+		assertArrayEquals(expected, spielbrett.ausgabe());
+	}
+	
+	@Test(expected = Exception.class)
+	public void zugUngueltigerSpieler() throws Exception {
+		spielbrett.zug(5, 0);
+		spielbrett.zug(2, 3);
+										
+		assertArrayEquals(getInitialField(), spielbrett.ausgabe());
 	}
 
+	@Test(expected = Exception.class)
+	public void zugUngueltigePosition() throws Exception {
+		spielbrett.zug(10, 1);
+		spielbrett.zug(-1, 2);
+		
+		assertArrayEquals(getInitialField(), spielbrett.ausgabe());
+	}
+	
+	@Test(expected = Exception.class)
+	public void zugBelegtesFeld() throws Exception{
+		spielbrett.zug(1, 1);
+		spielbrett.zug(1, 2);
+	}
+	
+	@Test
+	public void hasZahlReiheTest() throws Exception {
+		assertFalse(spielbrett.hasZahlEineReihe(1));
+		assertFalse(spielbrett.isJedesFeldBelegt());
+		spielbrett.zug(0, 1);
+		spielbrett.zug(1, 2);
+		spielbrett.zug(2, 1);
+		spielbrett.zug(3, 2);
+		spielbrett.zug(4, 1);
+		assertFalse(spielbrett.hasZahlEineReihe(2));
+		spielbrett.zug(5, 2);
+		spielbrett.zug(6, 1);
+		spielbrett.zug(7, 2);
+		spielbrett.zug(8, 1);
+		assertTrue(spielbrett.hasZahlEineReihe(1));
+		assertTrue(spielbrett.isJedesFeldBelegt());
+	}
+
+	private int[][] getInitialField() {
+		int[][] expected = new int[][]{ {0, 0, 0},
+										{0, 0, 0},
+										{0, 0, 0} };
+		return expected;
+	}
+	
 }

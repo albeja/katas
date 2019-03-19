17dcbe2cd86d82b27aeb24be3ce566a8a1c47558
Jannis von Albedyll <jvonalbedyll@andrena.de>
2019-03-19 10:10:51 +0100

    Handling last Frame added

--- katas/src/bowlingKata/BowlingGame.java
+++ katas/src/bowlingKata/BowlingGame.java
@@ -7,14 +7,47 @@ public class BowlingGame {
 
 	LinkedList<Frame> frames = new LinkedList<Frame>();
 	
-	public void addRoll(int pins) {
-		if(isFirstFrame() || getCurrentFrame().isFull() ) {
+	public void addRoll(int pins) throws Exception {
+		if(isOver()) {
+			throw new Exception("No roll left, game is over!");
+		}
+		
+		if(isFirstFrame() || (getCurrentFrame().isFull() && !isLastStrikeOrSpare()) ) {
 			frames.add(new Frame());
 		} 
+
+		if(isLastStrikeOrSpare()) {
+			frames.getLast().addBonusScore(pins);
+		}
 			
 		getCurrentFrame().addPins(pins);
+		
+		if(frameBeforeWasStrike()) {
+			frames.get(frames.size() - 2).addBonusScore(pins);
+		}
+		
+		if(frameBeforeWasSpare() && frames.getLast().getRolls() == 1) {
+			frames.get(frames.size() - 2).addBonusScore(pins);
+		}
 	}
 	
+	private boolean frameBeforeWasSpare() {
+		try {
+			return frames.get(frames.size() - 2).isSpare();			
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private boolean frameBeforeWasStrike() {
+		try {
+			return frames.get(frames.size() - 2).isStrike();			
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
 	private boolean isFirstFrame() {
 		return frames.isEmpty();
 	}
@@ -33,6 +66,10 @@ public class BowlingGame {
 		
 	public boolean isOver() {
 		return frames.size() == 10 && frames.getLast().isFull()
-				&& !frames.getLast().isSpare() && !frames.getLast().isStrike();
+				&& !frames.getLast().isSpare() && !frames.getLast().isStrike();	
+	}
+	
+	public boolean isLastStrikeOrSpare() {
+		return frames.size() == 10 && (frames.getLast().isSpare() || frames.getLast().isStrike());
 	}
 }
--- katas/src/bowlingKata/BowlingGameTest.java
+++ katas/src/bowlingKata/BowlingGameTest.java
@@ -16,26 +16,53 @@ public class BowlingGameTest {
 	}
 
 	@Test
-	public void testGameWithoutSparesOrStrikes() {
-		for(int rollCtr=0; rollCtr<10; rollCtr++) {
-			currentGame.addRoll(1);			
-		}
+	public void testScoreWithoutSparesOrStrikes() throws Exception {
+		addXRollsWithYPins(10, 1);
 		assertFalse(currentGame.isOver());
 		assertEquals(10, currentGame.getTotalScore());
 		
-		for(int rollCtr=0; rollCtr<10; rollCtr++) {
-			currentGame.addRoll(1);			
-		}
+		addXRollsWithYPins(10, 1);
 		assertTrue(currentGame.isOver());
 		assertEquals(20, currentGame.getTotalScore());
-		
+	}
+	
+	@Test
+	public void testPinsAndSizeOfFrames() throws Exception {		
 		Frame expectedFrame = new Frame();
 		expectedFrame.addPins(1);
 		expectedFrame.addPins(1);
 		
-		List<Frame> frames = currentGame.getFrames();
-		for(Frame frame : frames) {
+		addXRollsWithYPins(10, 1);
+		
+		List<Frame> currentFrames = currentGame.getFrames();
+		for(Frame frame : currentFrames) {
 			assertArrayEquals(expectedFrame.getPins(), frame.getPins());
+		}
+		
+		assertEquals(5, currentFrames.size());
+	}
+	
+	@Test (expected = Exception.class)
+	public void testNoExtraRollAfterEnd() throws Exception {
+		addXRollsWithYPins(20, 4);
+		
+		assertTrue(currentGame.isOver());
+		
+		currentGame.addRoll(4);
+	}
+	
+	@Test
+	public void testExtraRollAfterEnd() throws Exception {
+		addXRollsWithYPins(19, 4);
+		currentGame.addRoll(6);
+		currentGame.addRoll(5);
+		
+		assertEquals(20, currentGame.getFrames().get(9).getScore());
+	}
+	
+	private void addXRollsWithYPins(int rolls, int pins) throws Exception {
+		for(int rollCtr=0; rollCtr<rolls; rollCtr++) {
+			currentGame.addRoll(pins);			
 		}
 	}
 

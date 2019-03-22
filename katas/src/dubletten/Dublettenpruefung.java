ac8e01821aaaf14fa520214bf87b7c903cda11bd
Jannis von Albedyll <jvonalbedyll@andrena.de>
2019-03-22 10:29:50 +0100

    Dublettenpruefung refactored

--- katas/src/dubletten/Dublettenpruefung.java
+++ katas/src/dubletten/Dublettenpruefung.java
@@ -1,84 +1,47 @@
 package dubletten;
 
 import java.io.File;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Dublettenpruefung implements IDublettenpruefung {
-	private List<IDublette> kandidaten;
 	private List<File> filesUnterRoot;
-	private List<String> fileNamen;
-	private List<File> nachNameGefiltert;
-	private List<File> nachNameUndGrößeGefiltert;
+	private FileFinder finder = new FileFinder();
 	
 	@Override
-	public List<IDublette> sammleKandidaten(String pfad) {
-		initialisiereListen();
-		findeDateien(new File(pfad));
-		findeKandidatenAusListe();
-		
-		return kandidaten;
+	public List<IDublette> sammleKandidaten(String pfad) {		
+		filesUnterRoot = finder.findeDateien(pfad);
+		return findeKandidatenAusListe();
 	}
 
-	private void initialisiereListen() {
-		kandidaten = new LinkedList<IDublette>();
-		filesUnterRoot = new LinkedList<File>();
-		fileNamen = new LinkedList<String>();
-		nachNameGefiltert = new LinkedList<File>();
-		nachNameUndGrößeGefiltert = new LinkedList<File>();
-	}
+	private List<IDublette> findeKandidatenAusListe() {
+		List<IDublette> kandidatenListe = new LinkedList<IDublette>();
 
-	private void findeKandidatenAusListe() {
-		for(File file: filesUnterRoot) {
-			if (häufigkeitFilename(file) > 1) {
-				addeKandidaten(file);
-				fileNamen.remove(file.getName());
+		while(filesUnterRoot.isEmpty() == false) {
+			List<File> nachNameUndGrößeGefiltert = filtereListeNachNamenUndGröße(filesUnterRoot.get(0), filesUnterRoot);
+			
+			if (nachNameUndGrößeGefiltert.size() > 1) {
+				kandidatenListe.add(baueDublettenkandidat(nachNameUndGrößeGefiltert));
 			}
+			
+			filesUnterRoot.remove(0);
 		}
-	}
-
-	private int häufigkeitFilename(File file) {
-		return Collections.frequency(fileNamen, file.getName());
-	}
-
-	private void addeKandidaten(File file) {
-		Dublette dublette = new Dublette();
-
-		nachNameGefiltert = filtereListeNachNamen(file.getName());
-		nachNameUndGrößeGefiltert = filtereListeNachGröße(file.length());
 		
+		return kandidatenListe;
+	}
+
+	private Dublette baueDublettenkandidat(List<File> nachNameUndGrößeGefiltert) {
+		Dublette dublette = new Dublette();
 		for(File gefilterterFile : nachNameUndGrößeGefiltert) {
 			dublette.addDateipfad(gefilterterFile.getPath());
 		}
-
-		if(dublette.getDateipfade().size() > 1) kandidaten.add(dublette);
-	}
-
-	private void findeDateien(File basisPfad) {
-		if (!(basisPfad.listFiles() == null)) {
-			durchsucheVerzeichnis(basisPfad);	
-		}
-	}
-
-	private void durchsucheVerzeichnis(File basisPfad) {
-		for(File fileInDirectory : basisPfad.listFiles()) {
-			if(fileInDirectory.isFile()) {
-				filesUnterRoot.add(fileInDirectory);
-				fileNamen.add(fileInDirectory.getName());
-			} else {
-				findeDateien(fileInDirectory);
-			}
-		}
+		return dublette;
 	}
 	
-	private List<File> filtereListeNachGröße(long fileSize) {
-		return nachNameGefiltert.stream().filter(fileToFilter -> fileToFilter.length() == fileSize).collect(Collectors.toList());
-	}
-	
-	private List<File> filtereListeNachNamen(String fileName) {
-		return filesUnterRoot.stream().filter(fileToFiler -> fileToFiler.getName().equals(fileName)).collect(Collectors.toList());
+	private List<File> filtereListeNachNamenUndGröße(File file, List<File> fileList) {
+		List<File> nachNameGefiltert = fileList.stream().filter(fileToFiler -> fileToFiler.getName().equals(file.getName())).collect(Collectors.toList());
+		return nachNameGefiltert.stream().filter(fileToFilter -> fileToFilter.length() == file.length()).collect(Collectors.toList());
 	}
 
 }

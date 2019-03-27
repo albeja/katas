a8edab1c0928a69750d363ca489e4ae5c1d28479
Jannis von Albedyll <jvonalbedyll@andrena.de>
2019-03-27 13:25:37 +0100

    Refactored

--- katas/src/dubletten/DublettenprüfungGUI.java
+++ katas/src/dubletten/DublettenprüfungGUI.java
@@ -20,11 +20,8 @@ public class DublettenprüfungGUI extends javax.swing.JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-    private JTextField outputFileNameField;
-    private JLabel outputFileNameLabel;
-    private JButton pathChooseButton;
-    private JLabel startPath;
-    private JButton startButton;
+    private JTextField outputFileNameField = new JTextField("Absoluter Dateipfad", 20);
+    private JLabel startPath = new JLabel("Zu durchsuchender Pfad");
     
 	public DublettenprüfungGUI() {
 		init();
@@ -32,40 +29,29 @@ public class DublettenprüfungGUI extends javax.swing.JFrame {
 	}
 	
 	private void init() {
+		add(createPanel());
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Dublettenprüfung");
+        setSize(300, 200);
+        pack();
+	}
+
+	private JPanel createPanel() {
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(3,3, 10, 10));
+		setPanelLayout(panel);
 		
-        // Button Verzeichnissuche
-		pathChooseButton = new JButton("Wähle Pfad");
-		pathChooseButton.setPreferredSize(new Dimension(40, 20));
-		pathChooseButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser chooser = new JFileChooser();
-				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				int rueckgabeWert = chooser.showOpenDialog(null);
-				
-				if(rueckgabeWert == JFileChooser.APPROVE_OPTION) {
-					startPath.setText(chooser.getSelectedFile().getPath());
-				}
-				
-			}
-		});
-		panel.add(pathChooseButton);
-		
-		// Label ausgewähltes Verzeichnis
-		startPath = new JLabel("Zu durchsuchender Pfad");
+		panel.add(createPathChooseButton());
 		panel.add(startPath);
 		
-		// Label Ergebnisfile
-		outputFileNameLabel = new JLabel("Ergebnis speichern in: ");
-		panel.add(outputFileNameLabel);
-		
-		// Formfield Ergebnisfile
-		outputFileNameField = new JTextField("Absoluter Dateipfad", 20);
+		panel.add(new JLabel("Ergebnis speichern in: "));
 		panel.add(outputFileNameField);
 		
-        // Startbutton
-		startButton = new JButton("Start");
+		panel.add(createStartButton());
+		return panel;
+	}
+
+	private JButton createStartButton() {
+		JButton startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -78,15 +64,28 @@ public class DublettenprüfungGUI extends javax.swing.JFrame {
 				}
 			}
 		});
-		
-		panel.add(startButton);
-		
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Dublettenprüfung");
-        setSize(300, 200);
-        
-        add(panel);
-        pack();
+		return startButton;
+	}
+
+	private JButton createPathChooseButton() {
+		JButton pathChooseButton = new JButton("Wähle Pfad");
+		pathChooseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int rueckgabeWert = chooser.showOpenDialog(null);
+				
+				if(rueckgabeWert == JFileChooser.APPROVE_OPTION) {
+					startPath.setText(chooser.getSelectedFile().getPath());
+				}
+				
+			}
+		});
+		return pathChooseButton;
+	}
+
+	private void setPanelLayout(JPanel panel) {
+		panel.setLayout(new GridLayout(3,3, 10, 10));
 	}
 	
 	
